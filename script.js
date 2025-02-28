10e99e10a84f1d59885dbaa8fa2a296835c07fdd
Alexia Cervantes <Alexiacervantes120@gmail.com>
2025-02-27 22:17:08 -0800

    project desc. & placeholders

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.querySelector('.cta-button').addEventListener('click', () => {
     window.location.href = '#projects';
-});
+});//project button leads to projects section when clicked
 
 const form = document.getElementById('contact-form');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    alert('Message sent! I will get back to you soon.');
-});
+    // alert('Message sent! I will get back to you soon.'); 
+    alert('Message not sent! This feature is being worked on :( ');
+
+});//contact form
+
